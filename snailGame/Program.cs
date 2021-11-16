be8efeb2fb0bc480cbf5dd706ee884f7f0272f5b
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-11-16 11:05:01 +0100

    gooh

---
+++ snailGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+string place = "start";
+
+while (place != "exit" ) 
+{
+if (place == "start")
+{
+Console.WriteLine("You are currently in the docking station, there is a door ahead of you leading to the wheelhouse");
+Console.WriteLine("If you want to enter say the password, Sesameseed and if you do not want to enter say nothing.");
+string where = Console.ReadLine();
+where = where.ToLower();
+
+if (where == "yes")
+{
+    place = "wheelhouse";
+}
+
+else {
+ place = "start";
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
